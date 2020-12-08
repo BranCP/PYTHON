c2f6becd420bbbe2c7405821226a51458c539eb3
BranCP <brancpa@gmail.com>
2020-12-08 18:08:54 -0500

    commitlap

--- code3/emaildb.py
+++ code3/emaildb.py
@@ -1,3 +1,39 @@
+# import sqlite3
+
+# conn = sqlite3.connect('emaildb.sqlite')
+# cur = conn.cursor()
+
+# cur.execute('DROP TABLE IF EXISTS Counts')
+
+# cur.execute('''
+# CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+# fname = input('Enter file name: ') 
+# if (len(fname) < 1): fname = 'mbox-short.txt'
+# fh = open(fname)
+# for line in fh:
+#     if not line.startswith('From: '): continue
+#     pieces = line.split()
+#     email = pieces[1]
+#     cur.execute('SELECT count FROM Counts WHERE org = ? ', (email,))
+#     row = cur.fetchone()
+#     if row is None:
+#         cur.execute('''INSERT INTO Counts (org, count)
+#                 VALUES (?, 1)''', (email,))
+#     else:
+#         cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+#                     (email,))
+#     conn.commit()
+
+# # https://www.sqlite.org/lang_select.html
+# sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+# for row in cur.execute(sqlstr):
+#     print(str(row[0]), row[1])
+
+# cur.close()
+
+import re
 import sqlite3
 
 conn = sqlite3.connect('emaildb.sqlite')
@@ -6,27 +42,28 @@ cur = conn.cursor()
 cur.execute('DROP TABLE IF EXISTS Counts')
 
 cur.execute('''
-CREATE TABLE Counts (email TEXT, count INTEGER)''')
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
-fname = input('Enter file name: ')
+fname = 'D:\Cursos\PYTHON\PYTHON\code3\mbox.txt'
 if (len(fname) < 1): fname = 'mbox-short.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
-    email = pieces[1]
-    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    email = re.findall('@([\w.]+)',pieces[1])  
+    email=email[0]  
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (email,))
     row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO Counts (email, count)
+        cur.execute('''INSERT INTO Counts (org, count)
                 VALUES (?, 1)''', (email,))
     else:
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
                     (email,))
     conn.commit()
 
 # https://www.sqlite.org/lang_select.html
-sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
